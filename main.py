923f103f45da5449a73d1973aed15adbed79b3d7
Inés María O <inesmao@misena.edu.co>
2022-09-12 21:38:41 -0500

    práctica#1

---
+++ main.py
@@ -0,0 +1,20 @@
+# Ejemplos Variables
+"""
+mensaje = "Hola mundo estoy aprendiendo Python"
+print(mensaje)
+"""
+
+nombre = "Inés"
+apellido = "Oliveros"
+ciudad = "Duitama"
+celular = 3005793492
+"""
+print(nombre + " " + apellido + " " + ciudad + " " + str(celular))
+print(nombre,apellido,ciudad,celular)
+print(f"hola {nombre} {apellido} de la ciudad {ciudad} con celular {celular}")
+print("hola %s de %s" % (nombre, ciudad))
+"""
+
+# Leer datos desde teclado
+nombre = input("Digita tu nombre: ")
+print(nombre)
